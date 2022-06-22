572fd26ca289ab380cfc2772857dcf87f7c436e0
Phoenix Riggins <Phoenixriggins18@gmail.com>
2022-06-22 13:05:07 -0500

    Add Week 2 Homework

---
+++ Week2/pizzaPlaceHomework.js
@@ -0,0 +1,14 @@
+let pizzaPlace = "Phoenix' Pizza Spot";
+let numberOfToppings = 12;
+console.log(pizzaPlace);
+console.log(numberOfToppings);
+console.log(typeof pizzaPlace);
+console.log(typeof numberOfToppings);
+console.log(`"Have you heard that "
+ ${pizzaPlace} has 
+ ${numberOfToppings} "toppings?"`);
+if (numberOfToppings < 10 ){
+    console.log("Quality, not Quantity.");
+} else {
+    console.log("A whole lot of pizza.")
+}
